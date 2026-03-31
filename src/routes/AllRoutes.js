6a04c210f1184c51027f801573f79f65b1c53664
nayan kamal <11nayan11kamal11@gmail.com>
2026-03-31 11:07:36 +0530

    fist commit

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Layout from './Layout'
 import Home from '../pages/Home'
 import About from '../pages/About'
@@ -14,60 +14,59 @@ import Contact from '../pages/Contact'
 const AllRoutes = () => {
     return (
         <>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={
-                        <Layout>
-                            <Home />
-                        </Layout>
-                    }>
-                    </Route>
-                    
-                    <Route path='/about' element={
-                        <Layout>
-                            <About />
-                        </Layout>}>
-                    </Route>
-                    <Route path='/product' element={
-                        <Layout>
-                            <Product />
-                        </Layout>}>
-                    </Route>
-                    <Route path='/product/:id' element={
-                        <WithoutFooterLayout>
-                            <SingleProduct />
-                        </WithoutFooterLayout>}>
-                    </Route>
-                    <Route path='/blog' element={
-                        <Layout>
-                            <Blog />
-                        </Layout>}>
-                    </Route>
+            <Routes>
+                <Route path='/' element={
+                    <Layout>
+                        <Home />
+                    </Layout>
+                }>
+                </Route>
+
+                <Route path='/about' element={
+                    <Layout>
+                        <About />
+                    </Layout>}>
+                </Route>
+                <Route path='/product' element={
+                    <Layout>
+                        <Product />
+                    </Layout>}>
+                </Route>
+                <Route path='/product/:id' element={
+                    <WithoutFooterLayout>
+                        <SingleProduct />
+                    </WithoutFooterLayout>}>
+                </Route>
+                <Route path='/blog' element={
+                    <Layout>
+                        <Blog />
+                    </Layout>}>
+                </Route>
 
 
 
-                    <Route path='/contact' element={
-                        <WithoutFooterLayout>
-                            <Contact />
-                        </WithoutFooterLayout>}>
-                    </Route>
-
-                    
-                    <Route path='/register' element={
-                        <WithoutFooterLayout>
-                            <Register />
-                        </WithoutFooterLayout>}>
-                    </Route>
+                <Route path='/contact' element={
+                    <WithoutFooterLayout>
+                        <Contact />
+                    </WithoutFooterLayout>}>
+                </Route>
 
 
-                    <Route path='/login' element={
-                        <WithoutFooterLayout>
-                            <Login />
-                        </WithoutFooterLayout>}>
-                    </Route>
+                <Route path='/register' element={
+                    <WithoutFooterLayout>
+                        <Register />
+                    </WithoutFooterLayout>}>
+                </Route>
 
-                </Routes>
-            </BrowserRouter>
+
+                <Route path='/login' element={
+                    <WithoutFooterLayout>
+                        <Login />
+                    </WithoutFooterLayout>}>
+                </Route>
+
+            </Routes>
+
         </>
     )
 }
